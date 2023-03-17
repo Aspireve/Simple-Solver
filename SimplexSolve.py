5658d9f0a02d05e7793149bff9a67897b8812ec7
Steve <steve1818fernandes@gmail.com>
2023-03-17 22:42:39 +0530

    It finally Works, now just to take the user input

--- SimplexSolve.py
+++ SimplexSolve.py
@@ -9,60 +9,86 @@ def simplex() :
     # Check Inputs
 
     # Initialize Inputs
-    headers = ["Cb", "Basis", "b", "x1", "x2", "s1", "s2", "ratio" ]
-    no_of_eq = 2
-    obj_coe = {"x1" : 7.0, 
-            "x2" : 6.0, 
+    headers = ["Cb", "Basis", "b", "x1", "x2", "x3", "s1", "s2", "s3", "ratio" ]
+    no_of_eq = 3
+    obj_coe = {"x1" : 3.0, 
+            "x2" : 2.0, 
+            "x3" : 5.0,
             "s1" : 0.0, 
-            "s2" : 0.0}
-    st_coe = [[2.0, 4.0, 1.0, 0.0 ],
-            [3.0, 2.0, 0.0, 1.0]]
-    st_val = [16.0, 12.0]
-    basis = ["s1", "s2" ]
-    all_basis = ["x1", "x2", "s1", "s2"]
+            "s2" : 0.0,
+            "s3" : 0.0}
+    st_coe = [[1.0, 2.0, 1.0, 1.0, 0.0, 0.0 ],
+            [3.0, 0.0, 2.0, 0.0, 1.0, 0.0],
+            [1.0, 4.0, 0.0, 0.0, 0.0, 1.0]]
+    st_val = [430.0, 460.0, 420]
+    basis = ["s1", "s2", "s3" ]
+    all_basis = ["x1", "x2", "x3", "s1", "s2", "s3"]
     itr = 10
+    zj = []
     while itr:
+        if(itr > 0):
+            pass
         zj = calculateZj(st_coe, obj_coe, no_of_eq, basis, st_val)
         zjmcj = calculateZjmCj(zj, obj_coe,st_coe, all_basis)
-        key_col = calculateKey(zj)
+        key_col = calculateKeyCol(zjmcj)
         [ratio, key_row] = calculateKeyRow(no_of_eq,st_val, st_coe, key_col)
-        itr -= 1
+        
+        print(f"\n------ Iteration {itr} ------")
+
         printTable(headers, obj_coe, basis, st_val, st_coe, key_col, zj, zjmcj, no_of_eq, ratio)
-        print(key_col, key_row)
+        if (key_col == -1):
+            break
+        itr -= 1
         mul = st_coe[key_row][key_col]
-        for i in range(len(st_coe)):
+        for i in range(len(st_coe[0])):
             st_coe[key_row][i] = round(st_coe[key_row][i]/mul,2)
-        st_val[key_row] = round(st_val[key_row]/mul,2) 
+        st_val[key_row] = round(st_val[key_row]/mul, 2)
+        for i in range(len(st_coe)):
+            if( i != key_row):
+                factor = st_coe[i][key_col]
+                for j in range(len(st_coe[0])):
+                    st_coe[i][j] = round(st_coe[i][j] - (factor * st_coe[key_row][j]), 2)
+                st_val[i] = round(st_val[i] - (factor*st_val[key_row]))
+        
+        # st_val[key_row] = round(st_val[key_row]/mul,2) 
         basis[key_row] = all_basis[key_col]
+    print("Maxima Occurs at ")
+    for i in range(len(basis)):
+        print(basis[i], " : ", st_val[i])
+    print (f"Maximum Value is : {zj[0]}")
 
 
+# Calculates Zj properly
 def calculateZj(st_coe, obj_coe, no_of_eq, basis, st_val):
     zj = [0.0 for i in range(len(st_coe[0])+1)]
     for i in range(1,len(st_coe[0])+1):
         z = 0
         for j in range(no_of_eq):
-            z += obj_coe[basis[j]] * st_coe[j][i-1]
+            z = round(z + obj_coe[basis[j]] * st_coe[j][i-1], 2)
         zj[i] = z
     for j in range (no_of_eq):
-        zj[0] += obj_coe[basis[j]] * st_val[j]
+        zj[0] = round(zj[0] +  obj_coe[basis[j]] * st_val[j], 2)
     return zj
 
-def calculateKey(zj):
-    mi = zj[0]
+# Calculates Key Column Correctly
+def calculateKeyCol(zjmcj):
+    mi = zjmcj[0]
     index = 0
-    for i in range(len(zj)):
-        if(zj[i] < mi):
-            mi = zj[i]
+    for i in range(len(zjmcj)):
+        if(zjmcj[i] < mi):
+            mi = zjmcj[i]
             index = i
-    return index if mi<=0 else -1
+    return index if mi<0 else -1
     
-def calculateKeyRow(no_of_eq,st_val, st_coe, key_col):
+# Calculates Key Row Correctly 
+def calculateKeyRow(no_of_eq, st_val, st_coe, key_col):
     ratio = [0.0 for i in range(len(st_coe))]
     for i in range(no_of_eq):
         if(st_coe[i][key_col] == 0):
             ratio[i] = round(float('inf'),2)
         else:
             ratio[i] = round(st_val[i]/ st_coe[i][key_col],2)
+    
     mi = ratio[0]
     index = 0
     for i in range(len(ratio)):
@@ -71,6 +97,7 @@ def calculateKeyRow(no_of_eq,st_val, st_coe, key_col):
             index = i
     return [ratio, index]
 
+# Prints zjmcj correctly
 def calculateZjmCj(zj, obj_coe,st_coe, all_basis):
     zjmcj = [0.0 for i in range(len(st_coe[0]))]
     for i in range(len(zj)-1):
